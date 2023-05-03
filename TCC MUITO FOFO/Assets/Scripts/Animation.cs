90bab5e7135db240721923b3685711ccc1ce2b65
otsuaf17 <67385630+otsuaf17@users.noreply.github.com>
2023-05-02 21:43:15 -0300

    0.0.2
    Foi feito adicionado os sprites dele andando e parado.
    Foi feito os scripts de movimentação e o script responsável no controle de animações básicas do player.
    Foi feita a documentação de todos os códigos explicando cada um serve com funciona

---
+++ TCC MUITO FOFO/Assets/Scripts/Animation.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Script responsavel por controlar as animações no jogador
+
+public class Animation : MonoBehaviour
+{
+    Animator _animator; //Cria uma variavel da classe Animator
+    MovePlayer _player; //Cria uma variavel da classe MovePlayer
+
+    string currentState; //Cria uma variavel do tipo de texto responsavel pelo controle de estados do personagem
+    const string _idle = "PlayerIdle"; //variavel constante do tipo texto que define a animação do player ficar parado
+    const string _walk = "PlayerWalk"; //variavel constante do tipo texto que define a animação do player andar
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>(); //Pega os componentes do "Animator"
+        _player = FindAnyObjectByType<MovePlayer>(); //Procura e pega os componentes de um objeto que contenha o "MovePlayer" que no caso é o proprio player
+    }
+
+    // Update is called once per frame
+    void Update() //Executa a todo momento os metodos e funções que estejam na chaves do "Update"
+    {
+        MoveAnimation(); //Metodo responsavel em definir as animações de movimentação do player
+    }
+
+    void MoveAnimation()
+    {
+        //Se o player estiver em movimento ele vai executar a animação de andar
+        if(_player.xAxis != 0)
+        {
+            AnimationState(_walk);
+        }
+        //Se não o player vai executar a animação de quando ele estiver parado
+        else
+        {
+            AnimationState(_idle);
+        }
+    }
+
+    void AnimationState(string stateControl) //Metodo responsavel para controlar a animação 
+    {
+        if (currentState == stateControl) return; //Caso ele esteja executando a mesma animação ele não vai executar os codigos a baixo
+        
+        // Altera a animação do personagem para a animação especificada e atualiza o estado atual da animação.
+        _animator.Play(stateControl); 
+        currentState = stateControl; 
+    }
+}
--- TCC MUITO FOFO/Assets/Scripts/MovePlayer.cs
+++ TCC MUITO FOFO/Assets/Scripts/MovePlayer.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody2D Rb2D;
+    [SerializeField] float speedPlayer; //Variavel flutuante responsavel em definir a velocidade do player
+    [SerializeField] float speedJump; //Variavel flutuante responsavel em definir a velocidade de pulo do player
+    public float xAxis { get; set; } //Variavel flutuante responsavel em definir a direção do player
+
+
+    void Start() //Executa apenas uma vez quando inicia o jogo
     {
-        
+        Application.targetFrameRate = 60; //Codigo que tem como objetivo travar o jogo em 60 quadros por segundo
+        QualitySettings.vSyncCount = 0; //Codigo responsavel em desativar o vSync
+        Rb2D = GetComponent<Rigidbody2D>(); //Codigo responsavel em pegar os componetes da classe "Rigidbody2D"
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update() //Executa a todo momento
     {
-        
+        PlayerMove(); //Metodo responsavel por mover o player
+        FlipPlayer(); //Metodo responsavel por virar o player de acordo com a direção do mesmo
+    }
+
+    private void PlayerMove()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speedPlayer; //define a direção e a velociade do jogador
+
+        Rb2D.velocity = new Vector2(xAxis * speedPlayer, Rb2D.velocity.y); //Move o player
+
+    }
+    private void FlipPlayer()
+    {
+        //Se o player apertar a tecla "A" ou a seta da esquerda ele vai virar o player para esquerda
+        if(xAxis < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        //Caso ele aperte a tecla "D" ou a seta pra direita ele vai virar o player para direita
+        else if(xAxis > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
     }
 }
